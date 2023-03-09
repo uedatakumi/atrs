e0007533f6837263216bf9ca1fa6c7c74af5bb5f
Takumi Ueda <t-ueda@jaist.ac.jp>
2023-03-09 23:45:06 +0900

    Implement showTerm

--- src/TRS.hs
+++ src/TRS.hs
@@ -1,6 +1,14 @@
 module TRS where
 
 data Term = Var String | Con String | App Term Term
-  deriving (Eq, Show)
+  deriving Eq
 
 type TRS = [(Term, Term)]
+
+showTerm :: Term -> String
+showTerm (Var x)   = x
+showTerm (Con c)   = c
+showTerm (App t u) = "(" ++ showTerm t ++ " " ++ showTerm u ++ ")"
+
+instance Show Term where
+  show t = showTerm t
